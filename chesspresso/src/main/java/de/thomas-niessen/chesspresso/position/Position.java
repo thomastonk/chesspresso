598f8bbe943dc3fdda9d9d1f6e345ffd4103fe50
Thomas <Thomas@Eumel.speedport.ip>
2023-02-28 08:39:59 +0100

    Commit for 2022->2023 transition.

--- chesspresso/src/main/java/de/thomas-niessen/chesspresso/position/Position.java
+++ chesspresso/src/main/java/de/thomas-niessen/chesspresso/position/Position.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2021-2022 Thomas Niessen. All rights reserved.
+ * Copyright (C) 2021-2023 Thomas Niessen. All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
