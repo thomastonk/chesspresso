ca013afba4b3b2934412c5161f51a7157276a146
Thomas <Thomas@Eumel.speedport.ip>
2023-10-25 12:39:25 +0200

    Commit 25 October 2023.

--- chesspresso/src/main/java/de/thomas-niessen/chesspresso/position/view/Decoration.java
+++ chesspresso/src/main/java/de/thomas-niessen/chesspresso/position/view/Decoration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2019-2023 Thomas Niessen. All rights reserved.
+ * Copyright (C) 2019-2024 Thomas Niessen. All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
